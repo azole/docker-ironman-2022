70f5bc489dc65b9c9dbca9e51b76ac18bce3816f
ashley <ashleylai58@gmail.com>
2024-03-06 19:05:55 +0800

    add spring boot example

---
+++ workshop/examples/java-spring/src/main/java/school/docker/example/demo/HelloController.java
@@ -0,0 +1,12 @@
+package school.docker.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Docker!";
+    }
+}
---
+++ workshop/examples/java-spring/src/test/java/school/docker/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package school.docker.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
