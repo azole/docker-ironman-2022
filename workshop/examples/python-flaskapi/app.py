62765aaa9525068a5f46c55b83cfedd1cc84ee1c
ashley <ashleylai58@gmail.com>
2024-03-15 01:09:09 +0800

    update Dockerfile for flask

--- workshop/examples/python-flaskapi/app.py
+++ workshop/examples/python-flaskapi/app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello, Docker</h2>'
+    return '<h1>Hello, Flask in Docker</h2>'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
