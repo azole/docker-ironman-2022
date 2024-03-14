c6a3f0e6721009557b9ba063bfbc426ba1758d9d
ashley <ashleylai58@gmail.com>
2024-03-15 00:57:26 +0800

    update Dockerfile for java spring

--- workshop/examples/java-spring/src/main/java/school/docker/example/demo/HelloController.java
+++ workshop/examples/java-spring/src/main/java/school/docker/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Docker!";
+        return "Hello, Spring in Docker!";
     }
 }
